04d3874cecd6d43dde4e49bfe646264693161a79
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-11-13 18:00:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,10 +2,12 @@ class CashRegister
   
 attr_accessor :total 
 
-def initialize(discount = 0)
-  @discount = discount
+def initialize()
   total = 0 
   @total = total 
 end
+
+def discount(employee_discount)
+   @discount = employee_discount
 
 end
